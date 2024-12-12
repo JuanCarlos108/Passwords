4e8d05104bc0b9739199b368564929d0c07fbe33
JuanCarlos108 <patofachero501@gmail.com>
2024-12-11 20:34:36 -0600

    primer envio

---
+++ main.py
@@ -0,0 +1,10 @@
+import random as r , string as s
+
+elements = s.ascii_letters + s.ascii_lowercase + s.ascii_uppercase + s.punctuation + s.digits
+
+password = ""
+
+longitud = int(input("¿Que tan larga sera la variable?"))
+for i in range (longitud):
+    password += r.choice(elements)
+print(password)
